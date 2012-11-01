bb3ddf6ba4e1e9fba679b63d135698790d4a7599
Matt Mahoney <mattmahoneyfl@gmail.com>
2012-11-01 09:17:24 +0800

    lazy2 v1.00, lazy with an E8E9 filter (and 1 GB file size limit).

--- lazy2.cpp
+++ lazy2.cpp
@@ -1,58 +1,25 @@
-/* lazy.cpp v1.0 - LZ77 compressor
+/* lazy2.cpp v1.0 - LZ77 compressor
 
 (C) 2012 Dell Inc. Written by Matt Mahoney
 Licensed under GPL v3, http://www.gnu.org/copyleft/gpl.html
 
-lazy compresses and decompresses files in the LZ77 format.
+lazy2 compresses and decompresses files in the LZ77 format with e8e9
+preprocessing. Uncompressed files must be smaller than 2^30 bytes.
+Uses 1 GB memory.
 
-  Usage: lazy cmd input output
+  Usage: lazy2 cmd input output
 
 where cmd = 1..5 compresses faster..better, d decompresses.
 
-Compression prints statistics on the length distribution of literals and
-the length and offset distribution of matches. For example, a tar
-file of the Silesia corpus has the following statistics:
-
-> lazy 3 silesia.tar x
-
-   Range To        Literals    Matches    Offsets
--------- -------- --------- ---------- ----------
-       1 1           1979387          0      73405
-       2 3           1052317          0      25667
-       4 7            463352   13062258      48235
-       8 15           524935    3839590     280674
-      16 31            58529    1490365     798551
-      32 63             9642     591038     687947
-      64 127            3445     118826     852638
-     128 255            1775      26516     925461
-     256 511            1162      20403     994744
-     512 1023            891       3662    1087207
-    1024 2047            547       1260    1151070
-    2048 4095            328        391    1328039
-    4096 8191            133        228    1393830
-    8192 16383            42         41    1441973
-   16384 32767             5         13    1449732
-   32768 65535             2          1    1419433
-   65536 131071            0          0    1239571
-  131072 262143            0          0    1108786
-  262144 524287            0          0     967952
-  524288 1048575           0          0     740373
- 1048576 2097151           0          0     505221
- 2097152 4194303           0          0     324415
- 4194304 8388607           0          0     202399
- 8388608 16777215          0          0     107269
-Total                4096492   19154592
-
-211948544 -> 72760459 in 17.32 sec
-
-> lazy d x y
-72760459 -> 211948544 in 3.71 sec
-
-> fc/b silesia.tar y
-Comparing files silesia.tar and Y
-FC: no differences encountered
-
-(Times are on a 2.0 GHz T3200, Win32, on 1 of 2 cores).
+lazy2 differs from lazy in that it uses an e8e9 preprocessor and
+limits the file size so that it can all be read into 1 GB memory.
+The e8e9 preprocessor improves compression of x86 code in .exe and .dll
+files. To preprocess, the input is scanned in reverse order and for
+any 5 byte sequence of the form (E8|E9 xx xx xx 00|FF) beginning
+at offset i, the 3 middle bytes are interpreted as a 24 bit number
+LSB first and i is added modulo 2^24. This transform is applied
+prior to LZ77 compression. During decompression, the inverse transform
+is applied.
 
 In LZ77, data is compressed by coding strings that match previous
 strings by giving an offset of 1 or more to the previous copy, and
@@ -60,13 +27,13 @@ the number of bytes to copy. Characters not matched to previous
 output are coded uncompressed as literals, preceded by a length
 code of 1 or more.
 
-This distribution is typical. The number of bits needed to code an offset
+In a typical file, the number of bits needed to code an offset
 tends to have a uniform distribution, but the number of bits needed to
 code a literal or match length tends to have a geometric distribution.
 About 3/4 of the codes are matches. Thus, we use a 2 bit code (00) to
 indicate a literal, and a fixed length code of 5 bits, where the first
 2 bits are not 00, to code the number of bits in the offset (0..23).
-Literal and match lengths are coded using a marked binary format,
+Literal and match lengths are coded using an interleaved Elias Gamma format,
 where the leading 1 bit is removed and the remaining bits are preceded
 by a 1, and a 0 marks the end. For example, a literal length of 1abc
 (where a,b,c are 0 or 1) is coded as 1,a,1,b,1,c,0. For example,
@@ -119,15 +86,18 @@ Thus, memory usage by level is:
   4: 64 MB
   5: 96 MB
 
-Decompression always uses 16 MB. Higher compression levels compress
-slower but make decompression sligtly faster because there is less
-input to decompress. Some examples on silesia.tar:
-
-  1: 211948544 -> 77291986 in 11.01 sec
-  2: 211948544 -> 75132954 in 13.68 sec
-  3: 211948544 -> 72760459 in 17.32 sec
-  4: 211948544 -> 70852680 in 24.51 sec
-  5: 211948544 -> 69397506 in 36.91 sec
+Decompression uses memory equal to the output size. Higher compression
+levels compress slower but make decompression sligtly faster because there
+is less input to decompress. Some examples on silesia.tar, timed on a 2.0
+GHz T3200:
+                    lazy                    lazy2
+                  --------                --------  
+  1: 211948544 -> 77291986 in 11.01 sec   76952753 in 11.22 sec
+  2: 211948544 -> 75132954 in 13.68 sec   74794978 in 14.57 sec
+  3: 211948544 -> 72760459 in 17.32 sec   72422267 in 18.28 sec
+  4: 211948544 -> 70852680 in 24.51 sec   70514689 in 25.02 sec
+  5: 211948544 -> 69397506 in 36.91 sec   69059876 in 37.47 sec
+  5:              decompress   3.87 sec   decompress   4.10 sec
 
 During compression, the program searches each pointer in the hash
 bucket to find the longest match, and taking the closest in case
@@ -164,7 +134,7 @@ doesn't seem to have much effect on speed.
 #include <time.h>
 #include <assert.h>
 
-int tab[26][3]={{0}}; // literals, match length, offset counts by log n
+int tab[34][3]={{0}}; // literals, match length, offset counts by log n
 
 // return 0..32 bits in x not counting leading 0 bits
 int lg(unsigned x) {
@@ -200,9 +170,9 @@ void write_literal(unsigned char* buf, int i, int& lit, BitWriter& out) {
   assert(lit>=0);
   if (lit<1) return;
   int ll=lg(lit);
-  assert(ll>=1 && ll<=24);
+  assert(ll>=1 && ll<=32);
   ++tab[ll][0];
-  ++tab[25][0];
+  ++tab[33][0];
   out.put(0, 2);
   --ll;
   while (--ll>=0) {
@@ -216,15 +186,14 @@ void write_literal(unsigned char* buf, int i, int& lit, BitWriter& out) {
 // Write match sequence of given length and offset (off in 1..2^24)
 // jj,kkk,u,nn,m[jjkkk-8] = match length unn, offset 1m
 void write_match(int len, int off, BitWriter& out) {
-//  printf("match %d %d\n", off, len);
-  assert(len>=4 && len<(1<<24));
+  assert(len>=4);
   assert(off>=1 && off<(1<<24));
   int ll=lg(len);
-  assert(ll>=3 && ll<=24);
+  assert(ll>=3 && ll<=32);
   int lo=lg(off);
   assert(lo>=1 && lo<=24);
   ++tab[ll][1];
-  ++tab[25][1];
+  ++tab[33][1];
   ++tab[lo][2];
   --lo;
   out.put((lo>>3)+1, 2);
@@ -249,10 +218,10 @@ int main(int argc, char** argv) {
   int cmd=-1;  // 0=decompress, 1..5=compression level
   if (argc!=4 || (cmd=argv[1][0])!='d' && (cmd<'1' || cmd>'5')) {
     fprintf(stderr,
-      "lazy v1.0 (C) 2012, Dell Inc. Written by Matt Mahoney\n"
+      "lazy2 v1.0 (C) 2012, Dell Inc. Written by Matt Mahoney\n"
       "Licensed under GPL v3. http://www.gnu.org/copyleft/gpl.html\n"
-      "To compress:   lazy N input output  (N = 1..5 = fastest..best)\n"
-      "To decompress: lazy d input output\n");
+      "To compress:   lazy2 N input output  (N = 1..5 = fastest..best)\n"
+      "To decompress: lazy2 d input output\n");
     return 1;
   }
   if (cmd=='d') cmd=0;
@@ -264,42 +233,53 @@ int main(int argc, char** argv) {
   if (!in) return perror(argv[2]), 1;
   FILE* out=fopen(argv[3], "wb");
   if (!out) return perror(argv[3]), 1;
-  BitWriter bw;
-  if (cmd) bw.out=out;
-
-  // Allocate buf (uncompressed data) and hashtable ht (compression only)
-  // ht[h] low 24 bits points to buf[i..i+HASHORDER-1], high 8 bits is buf[i]
-  const int HTSIZE=1<<(19+cmd);   // hashtable size, must be a power of 2
-  const int BUFSIZE=1<<24;  // buffer size (each of 2), max 2^24
-  unsigned char* buf=(unsigned char*)calloc(BUFSIZE, 1+(cmd!=0));
-  int* ht=(int*)(cmd?calloc(HTSIZE+16, sizeof(int)):buf);
-  if (!buf || !ht) return fprintf(stderr, "Out of memory\n"), 1;
-  ht+=16-((ht-(int*)0)&15);  // align on 64 byte address
-  unsigned h=0;  // context hash of buf[i..]
+  const unsigned MAXBUF=1<<30;
 
   // Compress
-  while (cmd) {
+  if (cmd) {
 
-    // Read block into second half of buf
-    const int n=fread(buf+BUFSIZE, 1, BUFSIZE, in)+BUFSIZE;
-    if (n<=BUFSIZE) {
-      bw.flush();
-      break;
+    // Allocate buf (uncompressed data) and hashtable ht (compression only)
+    // ht[h] low 24 bits points to buf[i..i+HASHORDER-1], high 8 bits is buf[i]
+    const int BUFSIZE=1<<24;
+    const int HTSIZE=1<<(19+cmd);   // hashtable size, must be a power of 2
+    unsigned char* buf=(unsigned char*)calloc(MAXBUF, 1);
+    int* ht=(int*)calloc(HTSIZE+16, sizeof(int));
+    if (!buf || !ht) return fprintf(stderr, "Out of memory\n"), 1;
+    ht+=16-((ht-(int*)0)&15);  // align on 64 byte address
+    unsigned h=0;  // context hash of buf[i..]
+    BitWriter bw;
+    bw.out=out;
+
+    // Read block into buf
+    const int n=fread(buf, 1, MAXBUF, in);
+    if (n<0 || n>=int(MAXBUF)) fprintf(stderr, "File too big\n"), exit(1);
+
+    // e8e9 filter
+    int e8e9=0;
+    for (int i=n-5; i>=0; --i) {
+      if (((buf[i]&254)==0xe8) && ((buf[i+4]+1)&254)==0) {
+        unsigned a=(buf[i+1]|buf[i+2]<<8|buf[i+3]<<16)+i;
+        buf[i+1]=a;
+        buf[i+2]=a>>8;
+        buf[i+3]=a>>16;
+        ++e8e9;
+      }
     }
+    printf("%d e8e9 transforms\n", e8e9);
 
     // Scan the block just read in. ht may point to previous block
     const unsigned int HASHBUCKET=(1<<cmd); // searches per hash
     int lit=0;  // number of output literals pending
-    for (int i=BUFSIZE; i<n;) {
+    for (int i=0; i<n;) {
 
       // Search for longest match, or pick closest in case of tie
       int blen=0, bp=0, len=0;
       for (int k=0; k<int(HASHBUCKET); ++k) {
         int p=ht[h+k];
         if ((p>>24&255)==buf[i]) {  // compare in ht first
-          p=(p&BUFSIZE-1)+BUFSIZE;
+          p=(p&BUFSIZE-1)|(i&-BUFSIZE);
           if (p>=i) p-=BUFSIZE;
-          if (p>0 && p<i && p+(1<<24)>i) {
+          if (p>0 && p<i && p+BUFSIZE>i) {
             for (len=0; i+len<n && buf[p+len]==buf[i+len]; ++len);
             if (len>blen || len==blen && p>bp) blen=len, bp=p;
           }
@@ -337,10 +317,7 @@ int main(int argc, char** argv) {
 
     // Write pending literals at end of block
     write_literal(buf, n, lit, bw);
-
-    // Move data from second half of buf to first half if more input
-    // is expected.
-    if (n==BUFSIZE*2) memmove(buf, buf+BUFSIZE, BUFSIZE);
+    bw.flush();
   }
 
   // Decode. Syntax is:
@@ -348,16 +325,21 @@ int main(int argc, char** argv) {
   // jj,kkk,u,nn,m[jjkkk-8] = match length u*4+nn, offset 1m, jj>0
   // u is marked binary
   if (cmd==0) {
+
+    // Allocate output buffer
+    unsigned char* buf=(unsigned char*)calloc(MAXBUF, 1);
+    if (!buf) fprintf(stderr, "Out of memory\n"), exit(1);
+    
     unsigned state=0; // 0..4=expect new code, match len, offset, lit len, lit
     unsigned len=0;   // match or literal length
     unsigned ptr=0;   // next buf location to write
     unsigned bits=0;  // input bits, LSB first
     unsigned m=0;     // expected offset bits
-    unsigned n=0;     // number of bits in bits (0..32)
+    unsigned nb=0;    // number of bits in bits (0..32)
     int c;            // input byte
     while ((c=getc(in))!=EOF) {
-      bits+=c<<n;
-      n+=8;
+      bits+=c<<nb;
+      nb+=8;
       if (state==0) {  // expect new code
         len=1;
         if (bits&3) {  // match code jj,kkk
@@ -365,75 +347,90 @@ int main(int argc, char** argv) {
           bits>>=2;
           m+=bits&7;
           bits>>=3;
-          n-=5;
+          nb-=5;
           state=1;
         }
         else {  // literal, discard 00
           bits>>=2;
-          n-=2;
+          nb-=2;
           state=3;
         }
       }
-      while (state==1 && n>=3) {  // expect match length u,nn
+      while (state==1 && nb>=3) {  // expect match length u,nn
         if (bits&1) {
           bits>>=1;
           len+=len+(bits&1);
           bits>>=1;
-          n-=2;
+          nb-=2;
         }
         else {
           bits>>=1;
           len<<=2;
           len+=bits&3;
           bits>>=2;
-          n-=3;
+          nb-=3;
           state=2;
         }
       }
-      if (state==2 && n>=m) {  // expect m offset bits
+      if (state==2 && nb>=m) {  // expect m offset bits
         assert(m<24);
         assert(len>0);
         unsigned p=ptr-(bits&((1<<m)-1)|(1<<m));  // match location
+        assert(p<MAXBUF);
+        assert(p+len<=MAXBUF);
         while (len--)  // output match
-          putc(buf[ptr++&(BUFSIZE-1)]=buf[p++&(BUFSIZE-1)], out);
+          buf[ptr++]=buf[p++];
         bits>>=m;
-        n-=m;
+        nb-=m;
         state=0;
       }
-      while (state==3 && n>=2) {  // expect literal length
+      while (state==3 && nb>=2) {  // expect literal length
         assert(len>0);
         if (bits&1) {
           bits>>=1;
           len+=len+(bits&1);
           bits>>=1;
-          n-=2;
+          nb-=2;
         }
         else {
           bits>>=1;
-          --n;
+          --nb;
           state=4;
         }
       }
-      if (state==4 && n>=8) {  // expect len literals
+      if (state==4 && nb>=8) {  // expect len literals
         assert(len>0);
-        putc(buf[ptr++&(BUFSIZE-1)]=bits&255, out);
+        assert(ptr<MAXBUF);
+        assert(ptr+len<=MAXBUF);
+        buf[ptr++]=bits&255;
         bits>>=8;
-        n-=8;
+        nb-=8;
         if (--len<1) state=0;
-        assert(n<8);
+        assert(nb<8);
       }
+    }
+
+    // e8e9 filter
+    for (unsigned i=0; i<ptr; ++i) {
+      if (i+4<ptr && ((buf[i]&254)==0xe8) && ((buf[i+4]+1)&254)==0) {
+        unsigned a=(buf[i+1]|buf[i+2]<<8|buf[i+3]<<16)-i;
+        buf[i+1]=a;
+        buf[i+2]=a>>8;
+        buf[i+3]=a>>16;
+      }
+      putc(buf[i], out);
     }
   }
 
   // Print compression statistics
   if (cmd) {
     printf("\n"
-           "   Range To        Literals    Matches    Offsets\n"
-           "-------- -------- --------- ---------- ----------\n");
-    for (int i=1; i<25; ++i)
-      printf("%8d %-8d %10d %10d %10d\n",
+           "       Range To            Literals    Matches     Offsets\n"
+           "------------ ------------- --------- ---------- ----------\n");
+    for (int i=1; i<33; ++i)
+      printf("%12u %-12u %10d %10d %10d\n",
              1<<(i-1), (1<<i)-1, tab[i][0], tab[i][1], tab[i][2]);
-    printf("Total             %10d %10d\n\n", tab[25][0], tab[25][1]);
+    printf("Total                     %10d %10d\n\n", tab[33][0], tab[33][1]);
   }
   printf("%ld -> %ld in %1.2f sec\n", ftell(in), ftell(out),
     double(clock()-start)/CLOCKS_PER_SEC);
